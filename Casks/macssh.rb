2a209ca85eb8504bb4dfd75a240328d4a44c6dd1
Steve Shi <xuanlian@mac.com>
2026-03-21 20:47:36 +0800

    Add MacSSH cask v0.0.2

---
+++ Casks/macssh.rb
@@ -0,0 +1,17 @@
+cask "macssh" do
+  version "0.0.2"
+  sha256 :no_check
+
+  url "https://github.com/lpgneg19/MacSSH/releases/download/v#{version}/MacSSH-AppleSilicon.dmg"
+  name "MacSSH"
+  desc "A modern SSH & SFTP client for macOS powered by the Ghostty terminal engine"
+  homepage "https://github.com/lpgneg19/MacSSH"
+
+  app "MacSSH.app"
+
+  zap trash: [
+    "~/Library/Application Support/MacSSH",
+    "~/Library/Preferences/com.example.MacSSH.plist",
+    "~/Library/Saved Application State/com.example.MacSSH.savedState",
+  ]
+end
